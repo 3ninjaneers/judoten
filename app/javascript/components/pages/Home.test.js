83c6f4df7dc5f372738e34dac0717860855760a5
lancewrites <lancewrites77@gmail.com>
2022-07-13 14:59:54 -0700

    home page with testing and fixed some readability in the footer code

--- app/javascript/components/pages/Home.test.js
+++ app/javascript/components/pages/Home.test.js
@@ -4,3 +4,41 @@ import Adapter from 'enzyme-adapter-react-16'
 import Home from './Home.js'
 
 Enzyme.configure({ adapter: new Adapter() })
+
+describe("When Home renders", () => {
+    
+    it("displays a div", () => {
+      const home = shallow(<Home />)
+      const homeImg = home.find("img")
+      expect(homeImg.length).toEqual(1)
+    })
+    it("displays a button that says Find Dojo Near You", () => {
+        const home = shallow(<Home />)
+        const homeButton = home.find("Button")
+        expect(homeButton.length).toEqual(1)
+      })
+    
+    it("displays three Containers", () => {
+      const home = shallow(<Home />)
+      const homeContainers = home.find("Container")
+      expect(homeContainers.length).toEqual(5)
+    })
+    
+    it("displays h2 tag for FAQ and About sections", () => {
+      const home = shallow(<Home/>)
+      const homeHowWorks = home.find("h2")
+      expect(homeHowWorks.length).toEqual(2)
+    })
+    
+    it("displays 3 cards", () => {
+        const home = shallow(<Home/>)
+        const homeHowWorks = home.find("Card")
+        expect(homeHowWorks.length).toEqual(3)
+      })
+    
+    it("displays 4 dropdown questions", () => {
+        const home = shallow(<Home/>)
+        const homeQuestions = home.find("details")
+        expect(homeQuestions.length).toEqual(4)
+      })
+  })
