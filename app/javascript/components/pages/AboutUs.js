a6b512a1f3eb6906e9253dea6f0f86505606c0a2
lancewrites <lancewrites77@gmail.com>
2022-07-18 14:25:18 -0700

    changed the containers to cards still trying to figure it out

--- app/javascript/components/pages/AboutUs.js
+++ app/javascript/components/pages/AboutUs.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import { Container, Nav, NavItem, NavLink } from "reactstrap";
+import { Row, Col, Container, Nav, NavItem, NavLink } from "reactstrap";
 import githubLogo from "../../../assets/images/github.png";
-import linkedinLogo from "../../../assets/images/linkedin.png";
-import emailLogo from "../../../assets/images/email.png";
+// import linkedinLogo from "../../../assets/images/linkedin.png";
+// import emailLogo from "../../../assets/images/email.png";
 import {
   Card,
   CardImg,
@@ -10,8 +10,6 @@ import {
   CardBody,
   CardTitle,
   CardSubtitle,
-  Button,
-  Container,
 } from "reactstrap";
 export default class AboutUs extends Component {
   render() {
@@ -27,42 +25,33 @@ export default class AboutUs extends Component {
           </p>
         </Container>
         <Container id="profiles">
-          <Container className="profile">
-            <Container className="profile-header">
-              <h3>Name</h3>
-              <h4>Title/Second Title</h4>
-            </Container>
-            <Container className="profile-image-area col-2">
-              <img src="https://picsum.photos/200/200" alt="default" />
-            </Container>
-            <Container className="profile-content col-5">
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Provident expedita maiores et similique, fugit reprehenderit
-                dolorum quisquam placeat corporis dicta ad eaque, incidunt
-                explicabo aut labore? Natus a in quaerat!
-              </p>
-              <Container className="profile-socials">
-                <Nav>
-                  <NavItem>
-                    <NavLink>
-                      <img src={githubLogo} alt="github logo" />
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink>
-                      <img src={linkedinLogo} alt="Linkedin logo" />
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink>
-                      <img src={emailLogo} alt="Email logo" />
-                    </NavLink>
-                  </NavItem>
-                </Nav>
-              </Container>
-            </Container>
-          </Container>
+          <Card>
+            <CardBody>
+              <CardTitle>Name</CardTitle>
+              <CardSubtitle>Title/Secondary Title</CardSubtitle>
+              <Row>
+                <Col className="col-3">
+                  <CardImg
+                    center
+                    style={{ width: "15rem" }}
+                    src="https://picsum.photos/200/200"
+                    alt="default"
+                  />
+                </Col>
+                <Col className="col-9">
+                  <CardText className="profile-text">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Provident expedita maiores et similique, fugit reprehenderit
+                    dolorum quisquam placeat corporis dicta ad eaque, incidunt
+                    explicabo aut labore? Natus a in quaerat!
+                  </CardText>
+                </Col>
+              </Row>
+              <NavLink>
+                <img src={githubLogo} alt="github logo" />
+              </NavLink>
+            </CardBody>
+          </Card>
         </Container>
       </Container>
     );
