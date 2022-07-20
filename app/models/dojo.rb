18dd403c8959291cbd506d34ba7655ea458a8a64
lancewrites <lancewrites77@gmail.com>
2022-07-20 13:13:01 -0700

    some testing done

--- app/models/dojo.rb
+++ app/models/dojo.rb
@@ -9,6 +9,6 @@ class Dojo < ApplicationRecord
   validates :instructor, length: {minimum: 2}
   validates :email, length: {minimum: 5}
   validates :image, length: {minimum: 8}
-  
+  validates :address, uniqueness: true
   belongs_to :user
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should validate email" do
+    user = User.create(
+      email: nil,
+      encrypted_password: "1234567"
+    )
+    expect(user.errors[:email]).to_not be_empty
+  end
+  it "should validate password" do
+    user = User.new(
+      email: "first@user.com"
+    ).save
+    expect(user).to eq(false)
+  end
+  it "email should be unique" do
+    User.new(email:"first@user.com", encrypted_password: "1234567")
+    user2 = User.new(
+      email: "first@user.com", encrypted_password: "12345678"
+    ).save
+    expect(user2).to eq(false)
+  end
 end
